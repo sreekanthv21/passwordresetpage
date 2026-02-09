5b34cd12e2665bf296abfae76664070770b0fbc4
sreekanthv21 <kithuin21@gmail.com>
2026-02-09 13:03:22 +0530

    trial fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "e9f1d3c6350f4ba1cf0a8031dde6ae53
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "4265ad7f414bbd1777b62663b5d4316e",
+"flutter_bootstrap.js": "675be6c699a6e6bbf477550419bc4304",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
